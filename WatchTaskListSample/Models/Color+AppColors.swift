532484f59ec5aa6a1b9585c204420d0cca48ea42
Apple <>
2022-05-25 17:27:51 +0000

    Initial commit

---
+++ WatchTaskListSample/Models/Color+AppColors.swift
@@ -0,0 +1,20 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+App-specific colors.
+*/
+
+import SwiftUI
+
+extension Color {
+
+    /// The application's yellow color.
+    static let appYellow = Color("AppYellowColor")
+    
+    /// The application's cool gray color.
+    static let appCoolGray = Color("AppCoolGrayColor")
+    
+    /// The application's dark gray color.
+    static let appDarkGray = Color("AppDarkGrayColor")
+}
